1653b2eb4d86e24bc37482882fb20e5c716b1c9a
Adam Stein <adam@csh.rit.edu>
2020-08-23 15:12:24 -0400

    Added board members:
    	o web page templates (addition and update)
    	o models (addition and update)

--- apps/residents/urls.py
+++ apps/residents/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import ResidentListView
+from .views import BoardMembersListView, ResidentListView
 
 app_name = 'resident'
 urlpatterns = [
+    url(r'^board_members/$', BoardMembersListView.as_view(), name='board'),
     url(r'^information/$', ResidentListView.as_view(), name='information')
 ]
