ee86f4bfb29350be5cf62cfc9825c94b426480e1
Adam Stein <adam@csh.rit.edu>
2020-09-02 14:41:45 -0400

    Fixed two bugs in Gallery:
    	o not displaying HTML correctly in long description
    	o not filtering photos ONLY for the current person

--- hh/settings.py
+++ hh/settings.py
@@ -168,7 +168,7 @@ LOGIN_REDIRECT_URL = reverse_lazy('redirect')
 
 # Version information
 
-VERSION = '2.3.1'
+VERSION = '2.3.2'
 
 
 # List of settings to export to templates (django-settings-export)
