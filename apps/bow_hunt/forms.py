dfdb7a22beec1b45fc7ca62f9619be30608a17da
Adam Stein <adam@csh.rit.edu>
2022-05-03 09:40:12 -0400

    Added form to add log information

---
+++ apps/bow_hunt/forms.py
@@ -0,0 +1,145 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.utils import six
+from django.utils.encoding import force_text
+from django.utils.html import conditional_escape
+
+from .models import DataWarning, Location, Log, LogSheet
+
+from library.forms import MultipleSelectWithAdd, SelectWithAdd
+
+
+class HunterForm(forms.ModelForm):
+    location_id = forms.IntegerField(widget=forms.HiddenInput())    # Store only the ID, so we can avoid the large list
+    log_sheet_id = forms.IntegerField(widget=forms.HiddenInput())   # Store only the ID, so we can avoid the large list
+    pk = forms.IntegerField(required=False, widget=forms.HiddenInput())  # Need this to add instance to form later
+
+    class Meta:
+        exclude = ['location', 'log_sheet']
+        model = Log
+        widgets = {
+            'hunter': SelectWithAdd(),
+            'missing_warnings': forms.CheckboxSelectMultiple
+        }
+
+    def as_table(self):
+        return self._as_table('hunter') + \
+               self._as_table('deer_count') + \
+               self._as_table('deer_gender') + \
+               self._as_table('deer_points') + \
+               self._as_table('deer_tracking') + \
+               self._as_table('comment')
+
+    def clean(self):
+        # LogAdminForm.clean(), have them point to the same code doing the checking
+        cleaned_data = super().clean()
+        count = cleaned_data['deer_count']
+        gender = cleaned_data['deer_gender']
+        hunter = cleaned_data['hunter']
+        location_id = cleaned_data['location_id']
+        log_sheet_id = cleaned_data['log_sheet_id']
+        pk = cleaned_data.get('pk')
+        points = cleaned_data['deer_points']
+        tracking = cleaned_data['deer_tracking']
+
+        log_sheet = LogSheet.objects.get(id=log_sheet_id)
+        location = Location.objects.get(id=location_id)
+        if pk is not None:
+            self.instance = Log.objects.get(pk=pk)
+
+        # Database level restriction doesn't seem to work when Hunter = None, so we'll test for that here
+        if self.instance.id is None and hunter is None and \
+                Log.objects.filter(hunter=hunter, location=location, log_sheet=log_sheet).exists():
+            raise ValidationError('Log with this Log Sheet, Location and Hunter already exists.')
+
+        if location.year != log_sheet.date.year:
+            self.add_error(
+                'location',
+                f'Mismatch between location year ({location.year}) and log sheet year ({log_sheet.date.year})'
+            )
+
+        if count:
+            if hunter is None:
+                self.add_error(
+                    'hunter',
+                    'Hunter needs to be specified when any deer are shot or killed. If not specified, '
+                    'list first name as <unknown>.'
+                )
+
+            if gender is None:
+                self.add_error('deer_gender', 'Need to specify the deer gender when any are shot or killed')
+
+            if gender == Log.GENDER_MALE and points is None:
+                self.add_error('deer_points', 'Male deer need the number of points specified')
+
+            if tracking is None:
+                self.add_error(
+                    'deer_tracking', 'Need to specify if tracking was required when any deer are shot or killed'
+                )
+
+        return cleaned_data
+
+    def _as_table(self, field_name):
+        bf = self[field_name]
+        bf_errors = self.error_class([conditional_escape(error) for error in bf.errors])
+
+        if bf.label:
+            label = conditional_escape(force_text(bf.label))
+            label = bf.label_tag(label) or ''
+        else:
+            label = ''
+
+        if self.fields[field_name].help_text:
+            help_text = f'<span class="helptext">{force_text(self.fields[f"{field_name}"].help_text)}</span>'
+        else:
+            help_text = ''
+
+        error_row = f'<tr><td colspan="2">{force_text(bf_errors)}</td></tr>' if bf_errors else ''
+
+        if field_name == 'hunter':
+            missing_warnings = self['missing_warnings']
+
+            return f"""
+                {error_row}
+                <tr>
+                    <th>{label}</th>
+                    <td>
+                        {six.text_type(self['pk'])}
+                        {six.text_type(self['log_sheet_id'])}
+                        {six.text_type(self['location_id'])}
+                        {six.text_type(bf)}{help_text}
+                        {six.text_type(missing_warnings)}
+                    </td>
+                </tr>
+            """
+        else:
+            return f"""
+                {error_row}
+                <tr>
+                    <th>{label}</th>
+                    <td>{six.text_type(bf)}{help_text}</td>
+                </tr>
+            """
+
+
+HunterFormSet = forms.formset_factory(HunterForm, extra=0)
+
+
+class LocationForm(forms.Form):
+    location = forms.ModelMultipleChoiceField(
+        Location.objects.none(), label='', widget=MultipleSelectWithAdd(attrs={'size': 20})
+    )
+
+    def __init__(self, **kwargs):
+        self.year = kwargs.pop('year')
+        super().__init__()
+        self.fields['location'].queryset = Location.objects.filter(year=self.year)
+        self.initial['location'] = kwargs.get('initial', {'location': []})['location']
+
+    def as_p(self):
+        html = super().as_p()
+        return html.replace('<p>', '<p id="location_list">').replace(f'[{self.year}] ', '')
+
+
+class LogSheetForm(forms.Form):
+    log_sheet = forms.ModelChoiceField(LogSheet.objects.all(), widget=SelectWithAdd())
--- apps/bow_hunt/views.py
+++ apps/bow_hunt/views.py
@@ -1,11 +1,128 @@
+import json
+
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages import success
 from django.core import serializers
 from django.utils.html import escape
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.utils.safestring import mark_safe
+from django.views.generic import FormView, TemplateView
 
-from .models import LogSheet
+from .forms import HunterForm, HunterFormSet, LocationForm, LogSheetForm
+from .models import Location, Log, LogSheet
 
 from library.views.generic.mixins.ajax import AJAXResponseMixin
+
+
+class AddLogs(LoginRequiredMixin, FormView):
+    form_class = LogSheetForm
+    template_name = 'bow_hunt/add_logs.html'
+    success_url = reverse_lazy('bow_hunt:add_logs')
+
+    def form_valid(self, *args):
+        form, hunter_formset = args
+        log_sheet = form.cleaned_data['log_sheet']
+
+        # Save all the locations that had hunters
+        for hunter_form in hunter_formset.forms:
+            log = hunter_form.save(commit=False)
+            if hunter_form.cleaned_data['pk'] is None:
+                log.location = Location.objects.get(id=hunter_form.cleaned_data['location_id'])
+                log.log_sheet = log_sheet
+            log.save()
+            hunter_form.save_m2m()
+
+        num_logs = len(hunter_formset.forms)
+        success(
+            self.request,
+            f'Successfully added/updated {num_logs} log{"s"[:num_logs ^ 1]} to the {form.cleaned_data["log_sheet"]}'
+        )
+
+        return super().form_valid(form)
+
+    def form_invalid(self, *args):
+        form, hunter_formset, log_sheet_id = args
+
+        return self.render_to_response(
+            self.get_context_data(form=form, hunter_formset=hunter_formset, log_sheet_id=log_sheet_id)
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if 'hunter_formset' not in context:
+            context['hunter_formset'] = HunterFormSet()
+        else:
+            # We'll put the hunter formset forms into a format that will be easier to load on the page
+            data = []
+            for form in context['hunter_formset'].forms:
+                data.append({
+                    'form': str(form),
+                    'location_id': form.cleaned_data['location_id'],
+                    'no_hunter': form.cleaned_data['hunter'] is None
+                })
+            context['hunter_forms'] = json.dumps(data)
+
+        if 'log_sheet_id' in context:
+            location_ids = [self.request.POST[key] for key in self.request.POST if 'location_id' in key]
+            year = LogSheet.objects.get(id=context['log_sheet_id']).date.year
+
+            context['locations'] = mark_safe(str(LocationForm(initial={'location': location_ids}, year=year).as_p()))
+            initial = {'log_sheet': context['log_sheet_id']}
+        else:
+            initial = {}
+
+        context['log_sheet_form'] = LogSheetForm(initial=initial)
+
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        form.is_valid()     # The form for this view is always valid
+
+        hunter_formset = HunterFormSet(self.request.POST)
+
+        if hunter_formset.is_valid():
+            return self.form_valid(form, hunter_formset)
+        else:
+            return self.form_invalid(form, hunter_formset, self.request.POST['log_sheet'])
+
+
+class FetchLogSheetData(LoginRequiredMixin, AJAXResponseMixin, TemplateView):
+    content_type = 'application/json'
+
+    def get_context_data(self, **kwargs):
+        log_sheet = LogSheet.objects.get(id=self.request.GET['sheet_id'])
+        query = Log.objects.filter(log_sheet=log_sheet).values_list('location', flat=True)
+
+        log_forms = []
+        for index, log in enumerate(Log.objects.filter(log_sheet=log_sheet)):
+            # JavaScript/jQuery needs easy access to a few items, so we'll add an easy-to-get-to copy.
+            log_forms.append(
+                {
+                    'form': str(
+                        HunterForm(
+                            initial={
+                                'location_id': log.location_id,
+                                'log_sheet_id': log.log_sheet_id,
+                                'pk': log.pk
+                            },
+                            instance=log,
+                            prefix=f'form-{index}'
+                        )
+                    ),
+                    'location_id': log.location_id,
+                    'log_id': log.id,
+                    'no_hunter': log.hunter is None
+                }
+            )
+
+        context = {
+            'log_forms': log_forms,
+            'locations': str(LocationForm(initial={'location': query}, year=log_sheet.date.year).as_p())
+        }
+
+        return context
 
 
 class FetchLogSheetsByYear(LoginRequiredMixin, AJAXResponseMixin, TemplateView):
@@ -49,7 +166,15 @@ class FetchLogSheetsByYear(LoginRequiredMixin, AJAXResponseMixin, TemplateView):
                     prev_location = log.location
                 else:
                     # Another hunter at the same location
-                    log_data[-1]['hunters'].append(hunter)
+                    try:
+                        log_data[-1]['hunters'].append(hunter)
+                    except AttributeError as e:
+                        if log_data[-1]['hunters'] is None:
+                            e.args = (
+                                f'Data error: location "{log.location}" has multiple entries for {log_sheet.date} '
+                                'that shouldn\'t exist',
+                            )
+                        raise
 
             deer_count += log_sheet.deer_taken
 
