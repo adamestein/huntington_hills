0b2a9b240eb27a9429a390fb6649f23843600d46
Adam Stein <adam@csh.rit.edu>
2022-05-05 13:38:43 -0400

    Added the 'Bow Hunt' group and updated the bow hunt related views to ensure that only staff or people in the 'Bow Hunt' group have access
    Updated the residents menu to show bow hunt items if user is staff or in
    the 'Bow Hunt' group
    
    Updated version

--- apps/library/contrib/auth/mixins.py
+++ apps/library/contrib/auth/mixins.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
 
 
+class IsBowHuntMixin(UserPassesTestMixin):
+    def test_func(self):
+        # noinspection PyUnresolvedReferences
+        return self.request.user.is_staff or self.request.user.groups.filter(name='Bow Hunt').exists()
+
+
 class IsStaffMixin(UserPassesTestMixin):
     def test_func(self):
         # noinspection PyUnresolvedReferences
