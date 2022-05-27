bd30ac995c477c9e4ccf2d1a23693d2c7346f6a5
Adam Stein <adam@csh.rit.edu>
2022-05-27 16:26:44 -0400

    Added popup titles to hunter analysis web page
    Updated popup titles for location analysis web page
    
    Fixed bug in hunter analysis stats

--- apps/bow_hunt/reports/by_hunter.py
+++ apps/bow_hunt/reports/by_hunter.py
@@ -11,6 +11,10 @@ class Report(ReportBase):
 
         context = self.get_context_data(**kwargs)
 
+        # Because some deer are shot by unknown hunters, we need to count using the full Log, not just the
+        # filtered version
+        self.total_deer_shot = self.deer_count(Log.objects.filter(log_sheet__date__year__in=kwargs['years']))
+
         hunter_details, hunter_summaries = self._create_hunter_summaries(**kwargs)
 
         context.update({
@@ -37,7 +41,7 @@ class Report(ReportBase):
                 summary_days_hunted = hunter_logs.distinct().values_list('log_sheet').count()
                 summary_deer_shot = self.deer_count(hunter_logs)
                 summary_deer_tracked = hunter_logs.filter(deer__tracking=True).count()
-                summary_percent_shot = summary_deer_shot / summary_days_hunted * 100 if summary_deer_shot else 0
+                summary_percent_shot = summary_deer_shot / self.total_deer_shot * 100 if self.total_deer_shot else 0
                 summary_per_tracked = summary_deer_tracked / summary_deer_shot * 100 if summary_deer_shot else 0
 
                 summaries[year][hunter_name] = {
@@ -49,7 +53,7 @@ class Report(ReportBase):
                     'percent_tracked': summary_per_tracked
                 }
 
-                details[year][hunter_name] = {}
+                details[year][hunter_name] = {'normalized_name': hunter.name}
 
                 for location_id in hunter_logs.distinct().values_list('location', flat=True):
                     location_logs = hunter_logs.filter(location_id=location_id)
@@ -77,17 +81,13 @@ class Report(ReportBase):
         return details, summaries
 
     def _create_summary(self, **kwargs):
-        # Because some deer are shot by unknown hunters, we need to count using the full Log, not just the
-        # filtered version
-        total_deer_shot = self.deer_count(Log.objects.filter(log_sheet__date__year__in=kwargs['years']))
-
         summary = {
             'locations': self.logs.distinct().values_list('location').count(),
             'number_locs_deer_shot': self.logs.exclude(deer__isnull=True).distinct().values_list('location').count(),
             'number_hunters': len(kwargs['hunters']),
             'required_tracking': self.logs.filter(deer__tracking=True).count(),
             'total_days_hunted': self.logs.distinct().values_list('log_sheet').count(),
-            'total_deer_shot': total_deer_shot,
+            'total_deer_shot': self.total_deer_shot,
             'total_hunters': Hunter.objects.exclude(first_name='<unknown>').count(),
             'years': self.get_year_info(**kwargs)
         }
