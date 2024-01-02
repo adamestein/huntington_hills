79e06812981a135bd26d1f45cbe10e3b0206e9d9
Adam Stein <adam@csh.rit.edu>
2024-01-02 07:23:17 -0500

    Added capture and display of HH Commons Hunting exceptions (as per proxy doc)

---
+++ apps/bow_hunt/commons.py
@@ -0,0 +1,15 @@
+from itertools import groupby
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+
+from .models import HHCommonsHunting
+
+
+class CommonsHunting(LoginRequiredMixin, TemplateView):
+    template_name = 'bow_hunt/commons_hunting.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['exceptions'] = HHCommonsHunting.objects.all()
+        return context
