8ebe05498bb53b7bf11fadc14495da2b4d437aff
Adam Stein <adam@csh.rit.edu>
2025-04-06 13:36:09 -0400

    Fixed writing log message to indicate no new messages in mailbox (was always writing message when at end of message list)

--- apps/crontab/mailbox.py
+++ apps/crontab/mailbox.py
@@ -6,17 +6,22 @@ from django_mailbox.models import Mailbox
 
 
 def process_mailboxes():
+    # Uncomment to show all log messages in PyCharm console window when running this cronjob
+    # logging.basicConfig(level=logging.DEBUG)
+
     for mailbox in Mailbox.active_mailboxes.all():
         logger.info(f'Gathering messages for {mailbox.name}')
 
         messages = mailbox.get_new_mail()
+        num_msgs = 0
 
         try:
             for message in messages:
                 logger.info(f'Received {message.subject} (from {message.from_address})')
-            else:
-                logger.info(f'No new mail for {mailbox.name}')
+                num_msgs += 1
         except Exception as e:
             logger.error(f'{mailbox.name}: {e}')
 
+        if num_msgs == 0:
+            logger.info(f'No new mail for {mailbox.name}')
 
